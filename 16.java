413dacc6ff643b6a5c02a056765c6764dc845b06
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-17 10:03:11 +0530

    print the unrepeated number

---
+++ 16.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int num[]=new int[n];
+		for(int i=0;i<n;i++)
+		num[i]=s.nextInt();
+		int temp=0;
+		for(int i=0;i<n;i++)
+		{
+			int cnt=0;
+			for(int j=i+1;j<n;j++)
+			{
+				if(num[i]==num[j])
+				cnt++;
+			}
+			if(cnt==0)
+			temp=num[i];
+		}
+		System.out.print(temp);
+		
+		
+	}
+}
